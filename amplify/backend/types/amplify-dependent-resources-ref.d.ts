142554715718c956d2da9a0665f7ca4e4b359cdb
yjoon2k <syjoon2k@gmail.com>
2023-11-22 13:59:56 +0900

    graphql done, storage remain

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqldeb973e1": {
       "AppClientID": "string",
