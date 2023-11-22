6bc81cd90235888e2c9d8c0e9566e1560ebe3793
yjoon2k <syjoon2k@gmail.com>
2023-11-22 15:28:35 +0900

    error in localhost - [object object] error

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
+import { uploadData, getUrl, remove, getProperties } from "aws-amplify/storage";
 import config from "./amplifyconfiguration.json";
 import { generateClient } from "aws-amplify/api";
 import {
   Button,
   Flex,
   Heading,
+  Image,
   Text,
   TextField,
   View,
@@ -33,16 +35,35 @@ const App = ({ signOut }) => {
   async function fetchNotes() {
     const apiData = await client.graphql({ query: listTodos });
     const notesFromAPI = apiData.data.listTodos.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await JSON.stringify(getProperties(note.name));
+          note.image = url;
+        }
+        return note;
+      })
+    );
     setNotes(notesFromAPI);
   }
 
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
+    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
+      image: image.name,
     };
+    if (!!data.image) {
+      try {
+        const result = await uploadData(data.name, image).result;
+        console.log(result);
+      } catch (err) {
+        console.log(err);
+      }
+    }
     await client.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -51,9 +72,10 @@ const App = ({ signOut }) => {
     event.target.reset();
   }
 
-  async function deleteNote({ id }) {
+  async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
+    await remove(name);
     await client.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
@@ -64,6 +86,12 @@ const App = ({ signOut }) => {
     <View className="App">
       <Heading level={1}>My Notes App</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
+        <View
+          name="image"
+          as="input"
+          type="file"
+          style={{ alignSelf: "end" }}
+        />
         <Flex direction="row" justifyContent="center">
           <TextField
             name="name"
@@ -99,6 +127,13 @@ const App = ({ signOut }) => {
               {note.name}
             </Text>
             <Text as="span">{note.description}</Text>
+            {note.image && (
+              <Image
+                src={note.image}
+                alt={`visual aid for ${notes.name}`}
+                style={{ width: 400 }}
+              />
+            )}
             <Button variation="link" onClick={() => deleteNote(note)}>
               Delete note
             </Button>
